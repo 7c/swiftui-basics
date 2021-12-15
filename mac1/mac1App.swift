1909926425e65d29c0743f9587fc34786c7840d7
ot <o@o>
2021-12-15 15:19:06 -0500

    Initial Commit

---
+++ mac1/mac1App.swift
@@ -0,0 +1,17 @@
+//
+//  mac1App.swift
+//  mac1
+//
+//  Created by ot on 15.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct mac1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
